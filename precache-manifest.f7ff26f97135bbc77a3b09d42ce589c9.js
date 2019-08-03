196c7094652c1ac99c15cbea0bfd6fe41ea64c5b
Affiliatethis <chris@onecrookedtree.com>
2019-08-02 20:42:24 -0400

    Updates

---
+++ precache-manifest.f7ff26f97135bbc77a3b09d42ce589c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd31da06035946d9150ef08734dd34f",
+    "url": "/chrisksportfolio/index.html"
+  },
+  {
+    "revision": "ab2dc447b7741c7a3804",
+    "url": "/chrisksportfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/chrisksportfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ab2dc447b7741c7a3804",
+    "url": "/chrisksportfolio/static/js/main.911c42de.chunk.js"
+  },
+  {
+    "revision": "1a656ef515cef7c940f1",
+    "url": "/chrisksportfolio/static/js/runtime~main.5168095d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chrisksportfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
